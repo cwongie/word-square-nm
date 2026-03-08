56d7b41373f8b6b41739d258ac64529c56a5364e
Cherie Wong <cheriewong14499@gmail.com>
2026-03-08 22:06:58 +0000

    GREEN - Implemented getWordsOfLength in Dictionary

--- src/main/java/com/wordsquare/Dictionary.java
+++ src/main/java/com/wordsquare/Dictionary.java
@@ -4,8 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Dictionary {
 
@@ -33,4 +36,22 @@ public class Dictionary {
     public boolean contains(String word) {
         return words.contains(word.toLowerCase());
     }
+
+    public List<String> getWordsOfLength(int size, List<Character> letters) {
+        return words.stream()
+                .filter(word -> word.length() == size)
+                .filter(word -> canMakeWord(word, letters))
+                .collect(Collectors.toList());
+    }
+
+    public boolean canMakeWord(String word, List<Character> letters) {
+        List<Character> remainingLetters = new ArrayList<>(letters);
+        for (char c : word.toCharArray()) {
+            if (!remainingLetters.remove(Character.valueOf(c))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/test/java/com/wordsquare/DictionaryTest.java
+++ src/test/java/com/wordsquare/DictionaryTest.java
@@ -1,10 +1,9 @@
 package com.wordsquare;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 class DictionaryTest {
@@ -37,6 +36,6 @@ class DictionaryTest {
         assertTrue(result.contains("rose"));
         assertTrue(result.contains("oven"));
         assertTrue(result.contains("send"));
-        assertTrue(result.contains("Ends"));
+        assertTrue(result.contains("ends"));
     }
 }
